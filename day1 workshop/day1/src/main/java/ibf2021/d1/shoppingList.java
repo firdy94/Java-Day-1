62bc1ccc49e8132c850e8ea3c8ada6d3fd45311b
Firdaus <mohdfirdaus94@outlook.com>
2021-12-04 09:25:07 +0800

    Refactored shopping cart app and added setters and getters

--- day1 workshop/day1/src/main/java/ibf2021/d1/shoppingList.java
+++ day1 workshop/day1/src/main/java/ibf2021/d1/shoppingList.java
@@ -3,17 +3,19 @@ package ibf2021.d1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.io.Console;
+import java.util.Scanner;
 
 public class shoppingList {
+
     private List<String> itemsList=new ArrayList<>(0);
     private String input= "";
     private String [] inputList= new String[0];
+    private static final List<String> commands = new ArrayList<>(Arrays.asList("list","add", "delete"));
+
 
     public shoppingList(){
 
     }
-
     public shoppingList(List<String> itemsList, String input, String[] inputList){
         this.itemsList=itemsList;
         this.input=input;
@@ -24,15 +26,11 @@ public class shoppingList {
 
 
     public static void main(String[] args) {
-        List<String> commands = new ArrayList<>();
-        commands.add(0, "list");
-        commands.add(1, "add");
-        commands.add(2, "delete");
         shoppingList myList = new shoppingList();
 
         System.out.println("Welcome to your shopping cart");
         myList.checkInput();
-
+   
         while(!myList.input.contains("exit")){
 
             if((myList.input.equals(commands.get(0)))){
@@ -41,7 +39,6 @@ public class shoppingList {
                 }
             myList.getItem();
             myList.checkInput();
-
             }
             else if((myList.input.equals(commands.get(1)))){
                 for(int i=1;i<myList.inputList.length;i++){
@@ -54,26 +51,22 @@ public class shoppingList {
                 myList.checkInput();
             }
             else{
-
-            
                 System.out.println("Please enter one of the following commands:\nlist\nadd\ndelete\nexit");
                 myList.checkInput();
             }
-    
-            
-            }
         }
+    }
                 
 
     private void getItem(){
-        for(int i=0;i<this.itemsList.size();i++){
-            System.out.printf("%d. %s%n", i+1,this.itemsList.get(i));
+        for(int i=0;i<getItemsList().size();i++){
+            System.out.printf("%d. %s%n", i+1,getItemsList().get(i));
 
         }
     }
-    public void addItem(String item){
-         if (!this.itemsList.contains(item)){
-            this.itemsList.add(item);
+    private void addItem(String item){
+         if (!getItemsList().contains(item)){
+            getItemsList().add(item);
             System.out.printf("%s added to your your cart\n", item);
             }
         else{
@@ -81,29 +74,49 @@ public class shoppingList {
 
             }
     }
-    public void removeItem(int ind){
-        if(ind>=this.itemsList.size()){
+    private void removeItem(int ind){
+        if(ind>=getItemsList().size()){
             System.out.println("Incorrect item index\n");
 
         }
         else{
-            String removedItem= this.itemsList.remove(ind);
+            String removedItem= getItemsList().remove(ind);
             System.out.printf("%s removed from your cart\n", removedItem);
         }
         
     }
-
-    public void checkInput(){
-        this.input="";
-        this.inputList=new String[0];
-        Console cons= System.console();
-        input=cons.readLine(">");
-        this.inputList=this.input.trim().toLowerCase().split("[, ]+",0);
-        this.input=inputList[0];
+    private void checkInput(){
+        setInput("");
+        setInputList(new String[0]);
+        Scanner myScan = new Scanner (System.in);
+        System.out.print(">");
+        setInput(myScan.nextLine());
+        // Console cons= System.console();
+        // input=cons.readLine(">");
+        setInputList(getInput().trim().toLowerCase().split("[, ]+",0));
+        setInput(inputList[0]);
 
 
     }
  
-    }
 
+    public List<String> getItemsList() {
+        return this.itemsList;
+    }
+    public void setItemsList(List<String> itemsList) {
+        this.itemsList = itemsList;
+    }
+    public String getInput() {
+        return this.input;
+    }
+    public void setInput(String s) {
+        this.input = s;
+    }
+    public String[] getInputList() {
+        return this.inputList;
+    }
+    public void setInputList(String[] input) {
+        this.inputList = input;
+    }
+}
 
